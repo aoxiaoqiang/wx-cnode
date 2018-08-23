95184f28978005ee3ac44699197c9a35a1566fbf
敖晓强 <aoxiaoqiang@163.com>
2018-08-23 11:03:37 +0800

    feat: 添加评论列表

--- src/pages/topicDetail/topicDetail.js
+++ src/pages/topicDetail/topicDetail.js
@@ -2,7 +2,7 @@
 
 Page({
   data: {
-    detail: null
+    detail: null, // 帖子详情
   },
   onLoad(opts) {
     wx.fetch({
@@ -11,18 +11,22 @@ Page({
         mdrender: true
       }
     }).then((res) => {
-    	console.log(res)
-    	
+      // 日期
       res.data.data.create_at = wx.util.dateFomate(new Date(res.data.data.create_at), 'yyyy-MM-dd hh:mm:ss')
       res.data.data.last_reply_at = wx.util.dateFomate(new Date(res.data.data.last_reply_at), 'yyyy-MM-dd hh:mm:ss')
 
       // const md = new MarkdownIt()
       // let result = md.parse(res.data.data.content + '');
 
-      // console.log(result)
-
+      // 帖子详情, 内容处理
       res.data.data.content = res.data.data.content.replace(/<img/g, '<img style="width:100%;" ')
       res.data.data.content = res.data.data.content.replace(/<table/g, '<table style="width:100%;" ')
+
+      res.data.data.replies.map((reply) => {
+        reply.content = reply.content.replace(/<img/g, '<img style="width:100%;" ')
+        reply.content = reply.content.replace(/<table/g, '<table style="width:100%;" ')
+        return reply;
+      })
 
       this.setData({
         detail: res.data.data
